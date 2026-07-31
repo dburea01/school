81a80d3e823512c7b3b3c0acaed1bc8202b2a21f
domi <dburea01@gmail.com>
2026-07-31 11:52:55 +0200

    avatar_path => null for each user in the user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -42,12 +42,6 @@ class UserFactory extends Factory
         $firstName = str_replace(' ', '', $this->stripAccents(fake('fr_FR')->firstName($sex == 1 ? 'male' : 'female')));
         $lastName = str_replace(' ', '', $this->stripAccents(fake('fr_FR')->lastName()));
 
-        $avatarPaths = [
-            'https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?q=80&w=50&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=50&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            'https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?q=80&w=50&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        ];
-
         return [
             'role' => fake()->randomElement(UserRole::cases()),
             'status' => fake()->randomElement(UserStatus::cases()),
@@ -70,7 +64,7 @@ class UserFactory extends Factory
             'city' => fake('fr_FR')->city(),
             'country_id' => 'FR',
             'comment' => fake('fr_FR')->sentence(),
-            'avatar_path' => fake()->boolean(20) ? fake()->randomElement($avatarPaths) : null,
+            'avatar_path' => null,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
